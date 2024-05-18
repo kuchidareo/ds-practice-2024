d8ec9d781bcf938ca9289a2a61ff6339abc78083
kuchidareo <kuchidareo@gmail.com>
2024-05-18 09:59:32 +0300

    Split the multple nofraud noconflict scripts.

---
+++ cypress/e2e/multiple_nofraud_noconflict/multiple_nofraud_noconflict_2.cy.js
@@ -0,0 +1,27 @@
+describe('Multiple Order Test 2', () => {
+  it('places a non-fraudulent order successfully', () => {
+    cy.visit('localhost:8080/books/2')
+    cy.get('.btn').click()
+
+    // User Information
+    cy.get('#name').type('My name')
+    cy.get('#contact').type('123123123')
+    // Billing Address
+    cy.get('#street').type('My Street')
+    cy.get('#city').type('My City')
+    cy.get('#state').type('My State')
+    cy.get('#zip').type('12345')
+    cy.get('#country').select('Estonia')
+    // Payment Details
+    cy.get('#creditCardNumbe').type('2341234123412341')
+    cy.get('#creditCardExpirationDate').type('12/25')
+    cy.get('#creditCardCVV').type('123')
+    // Additional Information
+    cy.get('#shippingMethod').type('by ship')
+    cy.get('#termsAndConditions').check()
+
+    // Submit the form
+    cy.get('form').submit()
+    cy.url().should('include', '/confirmation')
+  })
+})
